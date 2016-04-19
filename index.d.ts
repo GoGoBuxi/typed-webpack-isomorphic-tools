a6904922340d03d78827bd6c9d807bd107f1f908
Sebastian Sobociński <hiddenboox@gmail.com>
2016-04-19 09:25:51 +0200

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,37 @@
+// Copyright (c) 2016
+//   ____        ____       ____             _
+//  / ___| ___  / ___| ___ | __ ) _   ___  _(_)
+// | |  _ / _ \| |  _ / _ \|  _ \| | | \ \/ / |
+// | |_| | (_) | |_| | (_) | |_) | |_| |>  <| |
+//  \____|\___/ \____|\___/|____/ \__,_/_/\_\_|
+//
+// Licensed under MIT.
+
+declare module 'webpack-isomorphic-tools' {
+    import * as webpackIsomorphicToolsPlugin from 'webpack-isomorphic-tools/plugin';
+
+    class WebpackIsomorphicTools {
+        constructor(configuration: webpackIsomorphicToolsPlugin);
+
+        development(isDevelopmentMode:boolean): WebpackIsomorphicTools;
+
+        server(context: string, callback: Function);
+
+        refresh(): void;
+
+        assets(): WebpackIsomorphicTools.Assets;
+    }
+
+    namespace WebpackIsomorphicTools {
+        interface Assets {
+            javascript?: Array<string>;
+            styles?: Array<string>;
+        }
+    }
+
+    export = WebpackIsomorphicTools;
+}
+
 declare module "webpack-isomorphic-tools/plugin" {
     import * as webpack from 'webpack';
 
